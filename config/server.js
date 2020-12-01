e252640bd45f2d9b9f4109aad8b90da3a6f712e8
cag <carlosaban@hotmail.com>
2020-12-01 18:05:56 -0500

    content type

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1c19c5773027302178e04fc4c201cfbf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4974e8a1-19ff-4f50-bfb1-73edaedfcafc'
+};
