142df21e9cca5ac08cc425c2f24eaf239707b323
cag <carlosaban@hotmail.com>
2020-12-03 15:31:04 -0500

    test automatic deploy

---
+++ config/database.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST', '50.87.146.219'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'suit_usrcabanto'),
+        username: env('DATABASE_USERNAME', 'suit_usrcabanto'),
+        password: env('DATABASE_PASSWORD', 'chino2021'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    },
+  },
+});
