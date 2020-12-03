ea70086736cb0ca806e1bf1e69a342af72a38535
cag <carlosaban@hotmail.com>
2020-12-03 16:08:48 -0500

    Updated Yarn lockfile

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: 'mysql',
         host: env('DATABASE_HOST', '50.87.146.219'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'suit_usrcabanto'),
+        database: env('DATABASE_NAME', 'suit_cabanto'),
         username: env('DATABASE_USERNAME', 'suit_usrcabanto'),
         password: env('DATABASE_PASSWORD', 'chino2021'),
         ssl: env.bool('DATABASE_SSL', false),
